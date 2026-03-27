f46d26636c88e71722c9967dec8e9b733a4c6da4
Anh Ngo <63425188+ticmyvom@users.noreply.github.com>
2026-03-26 20:25:24 -0700

    Add save and load features

--- hangman.rb
+++ hangman.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
+require 'base64'
+require 'fileutils'
+require 'json'
+
 # Hold most of the logic for this game
 class Hangman
-  attr_reader :remaining_turn, :word_guessed_in_full
-  attr_accessor :secret_word, :current_progress, :guessed_characters
+  attr_reader :remaining_turn
+  attr_accessor :secret_word, :current_progress, :guessed_characters, :word_guessed_in_full
 
   def initialize(turn = 8)
     # have a function that display the status depending on how many turns remain?
@@ -18,18 +22,24 @@ class Hangman
     word = ''
     lines = File.readlines(filepath)
     word = lines.sample.strip until word.length.between?(5, 12)
-    word
+    word.downcase
   end
 
   # return message
   def check_input(input)
-    if input == secret_word
+    return :character_guessed if guessed_characters.include?(input)
+
+    if input == 'save!'
+
+      :saved
+    elsif input == secret_word
       @current_progress = input.chars
       @word_guessed_in_full = true
+    elsif input.length == secret_word.length
+      @remaining_turn -= 1
+      guessed_characters.add(input)
     else
       input = sanitize_input(input)
-
-      return :character_guessed if guessed_characters.include?(input)
 
       indices = locate_indices(input)
       if indices.empty?
@@ -50,10 +60,45 @@ class Hangman
     current_progress.join == secret_word
   end
 
+  # discarding args
+  def to_json(*_args)
+    JSON.dump({
+                remaining_turn: @remaining_turn,
+                secret_word: Base64.strict_encode64(@secret_word),
+                # regular encode64 adds a line feed to every 60 encoded char which shouldn't be a problem
+                # since the secret word won't be that long
+                # but let's use strict anyway for better consistency.
+                current_progress: @current_progress,
+                word_guessed_in_full: @word_guessed_in_full,
+                guessed_characters: @guessed_characters.to_a # convert set to array for storage
+              })
+  end
+
+  def self.from_json(json)
+    json = JSON.parse(json)
+
+    hm = Hangman.new(json['remaining_turn'])
+    hm.secret_word = Base64.strict_decode64(json['secret_word'])
+    hm.current_progress = json['current_progress']
+    hm.word_guessed_in_full = json['word_guessed_in_full']
+    hm.guessed_characters = Set.new(json['guessed_characters'])
+
+    hm
+  end
+
+  def save_game
+    FileUtils.mkdir_p('./game_saves') # good - atomic and idempotent creation
+    filename = "#{current_progress.join}-#{secret_word.length}letters-#{remaining_turn}turnsleft.json"
+    f = File.new "./game_saves/#{filename}", 'w' # overwrite previous saved game if there is one
+    f.puts(to_json)
+    f.close
+  end
+
   private
 
   def sanitize_input(input)
-    return '' if input.length != 1
+    input.downcase!
+    return input[0] if input.length != 1
 
     input
   end
--- main.rb
+++ main.rb
@@ -2,29 +2,84 @@
 
 require_relative 'hangman'
 
-hm = Hangman.new
-while hm.remaining_turn.positive? && !hm.secret_word_guessed?
-  puts "You have #{hm.remaining_turn} remaining turns."
+saved_game_path = './game_saves'
 
-  puts 'Enter one character that you think would be in this word or the word itself:'
-  input = gets.chomp
+puts 'Welcome to Hangman.'
+puts 'See below and enter the number of your choice'
+puts '1. Play'
+puts '2. Load'
+puts '3. Exit'
 
-  status = hm.check_input(input)
-  case status
-  when :character_guessed
-    puts "You've guessed #{input} already."
+input = gets.chomp.to_i
+
+case input
+when 1
+  hm = Hangman.new
+when 2
+  current_saved_game = Dir.entries(saved_game_path)
+  # remove '..' and '.' from this array
+  current_saved_game.delete('..')
+  current_saved_game.delete('.')
+
+  if current_saved_game.empty?
+    puts "Nothing was saved previously. Let's start a new one.\n\n"
+    hm = Hangman.new
+  else
+    puts "Enter the index of the file that you'd like to load"
+    current_saved_game.each_with_index do |filename, index|
+      puts "#{index}. #{filename}"
+    end
+    index = gets.chomp.to_i
+    filename = current_saved_game.fetch(index, 'invalid')
+
+    until filename != 'invalid'
+      puts 'Index entered is invalid. Please try again:'
+      index = gets.chomp.to_i
+      filename = current_saved_game.fetch(index, 'invalid')
+    end
+
+    filename_path = "#{saved_game_path}/#{filename}"
+    json_string = File.readlines(filename_path)[0]
+    hm = Hangman.from_json(json_string)
   end
-  hm.view_progress
-  puts "Previous guesses: #{hm.guessed_characters.join(', ')}\n\n"
+when 3
+  puts 'Bye!'
+else
+  puts 'Invalid input. Exiting.'
 end
 
-if hm.secret_word_guessed?
-  if hm.word_guessed_in_full
-    puts 'Impressive, you guessed the word in full!'
-  else
-    puts 'Well done, you guessed it!'
+status = ''
+if [1, 2].include?(input)
+  while hm.remaining_turn.positive? && !hm.secret_word_guessed?
+    hm.view_progress
+
+    puts "You have #{hm.remaining_turn} turns remaining."
+    puts "Previous guesses: #{hm.guessed_characters.join(', ')}"
+    puts "Enter one character that you think would be in this word, the word itself, or 'save!' to save the game:"
+    input = gets.chomp
+
+    status = hm.check_input(input)
+    case status
+    when :character_guessed
+      puts "You've guessed #{input} already."
+    when :saved
+      puts 'Game saved!'
+      hm.save_game
+      break
+    end
+    puts "\n\n"
   end
-else
-  puts "No turns left to guess. The secret word is #{hm.secret_word}."
-  puts 'As a punishment, do dead hang for 5 seconds and then perform 2 repetitions of pull-up or one of its variants.'
+
+  if hm.secret_word_guessed?
+    if hm.word_guessed_in_full
+      puts 'Impressive, you guessed the word in full!'
+    else
+      puts "Well done, you've guessed it!"
+    end
+  else
+    unless status == :saved
+      puts "No turns left to guess. The secret word is #{hm.secret_word}."
+      puts 'As a punishment, do dead hang for 5 seconds and then perform 2 repetitions of pull-up or one of its variants.'
+    end
+  end
 end
